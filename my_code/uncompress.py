45e76e88e270c0fb29cc9941d21e4b0534db9d86
Jeffrey Sardina <jeffrey.sardina@gmail.com>
2021-06-07 11:55:12 +0100

    uncompression memory use fix--now streaming

--- my_code/uncompress.py
+++ my_code/uncompress.py
@@ -1,26 +1,30 @@
 import sys
 import pandas as pd
 
-def uncompress(predictions_file, compression_table, output):
-    #Load data
-    predictions = pd.read_csv(predictions_file, sep='\t', header=None, quotechar=None, quoting=3)
-
-    #Load compression table
+def load_table(compression_table):
+    print('ag lódáil an tábla')
     table = {}
     with open(compression_table, 'r') as inp:
         for line in inp:
             #Má tá níos mó na sin ann, tá earráid sa tábla féin
             idx, val = line.strip().split('\t')
             table[idx] = val
+    return table
 
-    #Decompress and output
-    print(predictions[0])
-    predictions[0] = predictions[0].apply(lambda x : table[x])
-    predictions.to_csv(output, header=False, index=False, sep='\t')
+def uncompress_stream(predictions_file, table, output):
+    print('ag díchomhbhrú')
+    with open(predictions_file, 'r') as inp:
+        with open(output, 'w') as out:
+            for line in inp:
+                compression_code, embedding = line.split('\t', 1)
+                uncompressed = table[compression_code]
+                new_line = uncompressed + '\t' + embedding
+                print(new_line, end='', file=out)
 
 if __name__ == '__main__':
     predictions_file = sys.argv[1]
     compression_table = sys.argv[2]
     output = sys.argv[3]
-    uncompress(predictions_file, compression_table, output)
+    table = load_table(compression_table)
+    uncompress_stream(predictions_file, table, output)
     exit(0)
