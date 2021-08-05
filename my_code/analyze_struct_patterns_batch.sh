480df68e24514d2d75d5baa09ee57ee52247ce0d
Jeffrey Sardina <jeffrey.sardina@gmail.com>
2021-08-05 21:33:31 +0100

    writeup backup and timestamp

--- my_code/analyze_struct_patterns_batch.sh
+++ my_code/analyze_struct_patterns_batch.sh
@@ -161,34 +161,99 @@
 
 #Evals
 #Cross vals
-echo 'combined_r1_bioconf'
+# echo 'combined_r1_bioconf'
+# python analyze_graph_struct_patterns_new.py \
+#     metric_analysis/struct_to_r1_or_auc/rounds_2_sinks_cent_separate/bioconf_to_r1_selected.csv \
+#     2 \
+#     r1 \
+#     combined_r1_bioconf \
+#     1e-2
+
+# echo 'combined_auc_bioconf'
+# python analyze_graph_struct_patterns_new.py \
+#     metric_analysis/struct_to_r1_or_auc/rounds_2_sinks_cent_separate/bioconf_to_auc_selected.csv \
+#     2 \
+#     AUC \
+#     combined_auc_bioconf \
+#     1e-3
+
+# echo 'combined_r1_general'
+# python analyze_graph_struct_patterns_new.py \
+#     metric_analysis/struct_to_r1_or_auc/rounds_2_sinks_cent_separate/general_to_r1_selected.csv \
+#     2 \
+#     r1 \
+#     combined_r1_general \
+#     1e-3
+
+# echo 'combined_auc_general'
+# python analyze_graph_struct_patterns_new.py \
+#     metric_analysis/struct_to_r1_or_auc/rounds_2_sinks_cent_separate/general_to_auc_selected.csv \
+#     2 \
+#     AUC \
+#     combined_auc_general \
+#     1e-3
+
+
+
+# #6.4.2 Round 1
+# #Cross vals
+# echo 'cent_r1_diff'
+# python analyze_graph_struct_patterns_new.py \
+#     metric_analysis/struct_to_r1_or_auc/round_1_to_difference/centrality_stats.csv \
+#     1 \
+#     r1 \
+#     cent_r1_diff
+
+# echo 'cent_auc_diff'
+# python analyze_graph_struct_patterns_new.py \
+#     metric_analysis/struct_to_r1_or_auc/round_1_to_difference/centrality_stats.csv \
+#     1 \
+#     AUC \
+#     cent_auc_diff
+
+# echo 'sink_r1_diff'
+# python analyze_graph_struct_patterns_new.py \
+#     metric_analysis/struct_to_r1_or_auc/round_1_to_difference/sinks_stats.csv \
+#     1 \
+#     r1 \
+#     sink_r1_diff
+
+# echo 'sink_auc_diff'
+# python analyze_graph_struct_patterns_new.py \
+#     metric_analysis/struct_to_r1_or_auc/round_1_to_difference/sinks_stats.csv \
+#     1 \
+#     AUC \
+#     sink_auc_diff
+
+# Evals
+echo 'cent_r1_diff'
 python analyze_graph_struct_patterns_new.py \
-    metric_analysis/struct_to_r1_or_auc/rounds_2_sinks_cent_separate/bioconf_to_r1_selected.csv \
+    metric_analysis/struct_to_r1_or_auc/round_1_to_difference/centrality_stats.csv \
     2 \
     r1 \
-    combined_r1_bioconf \
-    1e-2
+    cent_r1_diff \
+    1e-1
 
-echo 'combined_auc_bioconf'
+echo 'cent_auc_diff'
 python analyze_graph_struct_patterns_new.py \
-    metric_analysis/struct_to_r1_or_auc/rounds_2_sinks_cent_separate/bioconf_to_auc_selected.csv \
+    metric_analysis/struct_to_r1_or_auc/round_1_to_difference/centrality_stats.csv \
     2 \
     AUC \
-    combined_auc_bioconf \
-    1e-3
+    cent_auc_diff \
+    0.5
 
-echo 'combined_r1_general'
+echo 'sink_r1_diff'
 python analyze_graph_struct_patterns_new.py \
-    metric_analysis/struct_to_r1_or_auc/rounds_2_sinks_cent_separate/general_to_r1_selected.csv \
+    metric_analysis/struct_to_r1_or_auc/round_1_to_difference/sinks_stats.csv \
     2 \
     r1 \
-    combined_r1_general \
-    1e-3
+    sink_r1_diff \
+    1e-1
 
-echo 'combined_auc_general'
+echo 'sink_auc_diff'
 python analyze_graph_struct_patterns_new.py \
-    metric_analysis/struct_to_r1_or_auc/rounds_2_sinks_cent_separate/general_to_auc_selected.csv \
+    metric_analysis/struct_to_r1_or_auc/round_1_to_difference/sinks_stats.csv \
     2 \
     AUC \
-    combined_auc_general \
-    1e-3
+    sink_auc_diff \
+    0.5
